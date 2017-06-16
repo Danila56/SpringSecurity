0b71f1faa2da2112bafb9e189378806992759b91
Danila56 <danila.00.10@mail.ru>
2017-06-16 16:14:55 +0300

    Spring Security

---
+++ src/main/java/com/controller/ControllerSecurity.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControllerSecurity {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main(){
+        return new ModelAndView("index");
+    }
+}
